99f5bae4c2edd613cf79fafcc41870aa5a5171f3
daniel <daniel.hefele@smail.inf.h-brs.de>
2023-10-12 13:35:03 +0200

    erste erstellung

---
+++ src/org/hbrs/se1/ws23/uebung1/control/Zusatz.java
@@ -0,0 +1,14 @@
+package org.hbrs.se1.ws23.uebung1.control;
+
+public class Zusatz{
+    /**
+     * factory Method pattern kapitel 6 GoF Pattern
+     * Problem: Inkosistente Objekt-Erzeugung und Paratmetrisierung
+     * Lösung: Service-Klasse für die zentrale und konsistente Erzeugung
+     */
+
+    public static Translator create(){
+        Translator translator = new GermanTranslator();
+        return translator;
+    }
+}
